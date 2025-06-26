33b10690a12ae082cff96c32cced823a890bdc4b
pycharm <xiaofei0195@qq.com>
2025-06-27 00:54:23 +0800

    修改GoogleAnalytics的id

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -8,14 +8,14 @@ export default function GoogleAnalytics() {
     window.dataLayer = window.dataLayer || [];
     function gtag(){ dataLayer.push(arguments); }
     gtag('js', new Date());
-    gtag('config', 'G-QMZG2NQVHR');
+    gtag('config', 'G-8H8PCFCN9C');
   }, []);
 
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-QMZG2NQVHR`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-8H8PCFCN9C`}
       />
     </>
   );
